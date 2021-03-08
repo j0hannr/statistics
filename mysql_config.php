3a8d87b484db48a71a3f1c4f69c18c3021561521
j0hannr <johann.rohn@gmail.com>
2021-03-08 15:42:32 +0100

    git ignore file

--- mysql_config.php
+++ mysql_config.php
@@ -1,20 +1,5 @@
 <?php
 
-// LOCALHOST
-
-// $mysqlhost="localhost"; // MySQL-Host angeben
-
-// $mysqluser="root"; // MySQL-User angeben
-
-// $mysqlpwd="root"; // Passwort angeben
-
-// $connection= mysqli_connect($mysqlhost, $mysqluser, $mysqlpwd) or die ('Verbindungsversuch fehlgeschlagen');
-
-// $mysqldb="statistics"; // Gewuenschte Datenbank angeben
-
-// mysqli_select_db($mysqldb, $connection) or die('Konnte Datenbank nicht waehlen');
-
-
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 try {
     # host, user , password, database name
@@ -27,49 +12,3 @@ try {
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-
-
-// ONE SQUARED
-
-//    $mysqlhost="one-squared.com"; // MySQL-Host angeben
-//
-//    $mysqluser="statistics_2"; // MySQL-User angeben
-//
-//    $mysqlpwd="qAf#bAoJtt747gog"; // Passwort angeben
-//
-//    $connection=mysql_connect($mysqlhost, $mysqluser, $mysqlpwd) or die ('Verbindungsversuch fehlgeschlagen');
-//
-//    $mysqldb="statistics_2"; // Gewuenschte Datenbank angeben
-//
-//    mysql_select_db($mysqldb, $connection) or die('Konnte Datenbank nicht waehlen');
-
-// MORITZ STRATO
-
-//    $mysqlhost="rdbms.strato.de"; // MySQL-Host angeben
-//
-//    $mysqluser="U3419139"; // MySQL-User angeben
-//
-//    $mysqlpwd="lkrsakjnq3!x3q"; // Passwort angeben
-//
-//    $connection=mysql_connect($mysqlhost, $mysqluser, $mysqlpwd) or die ('Verbindungsversuch fehlgeschlagen');
-//
-//    $mysqldb="DB3419139"; // Gewuenschte Datenbank angeben
-//
-//    mysql_select_db($mysqldb, $connection) or die('Konnte Datenbank nicht waehlen');
-//    
-
-// ----- ONE SQUARED
-    
-//    $mysqlhost="one-squared.com:3306"; // MySQL-Host angeben
-//
-//    $mysqluser="statistics_2"; // MySQL-User angeben
-//
-//    $mysqlpwd="p90d@9DsQCgccrko"; // Passwort angeben
-//
-//    $connection=mysql_connect($mysqlhost, $mysqluser, $mysqlpwd) or die ('Verbindungsversuch fehlgeschlagen');
-//
-//    $mysqldb="statistics_2"; // Gewuenschte Datenbank angeben
-//
-//    mysql_select_db($mysqldb, $connection) or die('Konnte Datenbank nicht waehlen');
-    
-?>
