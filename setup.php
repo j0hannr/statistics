3c7f50af1a315135db2dd908731885367f3f04d0
j0hannr <johann.rohn@gmail.com>
2021-03-27 22:10:42 +0100

    trial

--- setup.php
+++ setup.php
@@ -15,21 +15,83 @@
 
   <body>
   <?php
-  echo "<br>Path: ".$_SERVER['DOCUMENT_ROOT'];
-  echo "<br>Path: ".$_SERVER['SCRIPT_FILENAME'];
-  echo "<br>Path: ".getcwd();
+//   echo "<br>Path: ".$_SERVER['DOCUMENT_ROOT'];
+//   echo "<br>Path: ".$_SERVER['SCRIPT_FILENAME'];
+//   echo "<br>Path: ".getcwd();
 
-  $rootPath = $_SERVER['DOCUMENT_ROOT'];
-$thisPath = dirname($_SERVER['PHP_SELF']);
-$onlyPath = str_replace($rootPath, '', $thisPath);
+//   $rootPath = $_SERVER['DOCUMENT_ROOT'];
+// $thisPath = dirname($_SERVER['PHP_SELF']);
+// $onlyPath = str_replace($rootPath, '', $thisPath);
 
 
-echo "<br>Path: ".$onlyPath;
+// echo "<br>Path: ".$onlyPath;
 
-$url = 'http://google.com/dhasjkdas/sadsdds/sdda/sdads.html';
-$url = $_SERVER['DOCUMENT_ROOT'];
-$parse = parse_url($url);
-echo "<br>".$parse['host'];
+// echo "<br>Path: ".$_SERVER['SERVER_NAME'];
+
+// echo "<br>Path: ".defined('__DIR__');
+
+
+
+// $url = 'http://google.com/dhasjkdas/sadsdds/sdda/sdads.html';
+// $url = $_SERVER['DOCUMENT_ROOT'];
+// $parse = parse_url($url);
+// echo "<br>".$parse['host'];
+
+echo "<br>Path: ".$_SERVER['PHP_SELF'];
+echo "<br>Path: ".$_SERVER['HTTP_HOST'];
+// $dir = 
+
+$indicesServer = array('PHP_SELF', 
+'argv', 
+'argc', 
+'GATEWAY_INTERFACE', 
+'SERVER_ADDR', 
+'SERVER_NAME', 
+'SERVER_SOFTWARE', 
+'SERVER_PROTOCOL', 
+'REQUEST_METHOD', 
+'REQUEST_TIME', 
+'REQUEST_TIME_FLOAT', 
+'QUERY_STRING', 
+'DOCUMENT_ROOT', 
+'HTTP_ACCEPT', 
+'HTTP_ACCEPT_CHARSET', 
+'HTTP_ACCEPT_ENCODING', 
+'HTTP_ACCEPT_LANGUAGE', 
+'HTTP_CONNECTION', 
+'HTTP_HOST', 
+'HTTP_REFERER', 
+'HTTP_USER_AGENT', 
+'HTTPS', 
+'REMOTE_ADDR', 
+'REMOTE_HOST', 
+'REMOTE_PORT', 
+'REMOTE_USER', 
+'REDIRECT_REMOTE_USER', 
+'SCRIPT_FILENAME', 
+'SERVER_ADMIN', 
+'SERVER_PORT', 
+'SERVER_SIGNATURE', 
+'PATH_TRANSLATED', 
+'SCRIPT_NAME', 
+'REQUEST_URI', 
+'PHP_AUTH_DIGEST', 
+'PHP_AUTH_USER', 
+'PHP_AUTH_PW', 
+'AUTH_TYPE', 
+'PATH_INFO', 
+'ORIG_PATH_INFO') ; 
+
+echo '<table cellpadding="10">' ; 
+foreach ($indicesServer as $arg) { 
+    if (isset($_SERVER[$arg])) { 
+        echo '<tr><td>'.$arg.'</td><td>' . $_SERVER[$arg] . '</td></tr>' ; 
+    } 
+    else { 
+        echo '<tr><td>'.$arg.'</td><td>-</td></tr>' ; 
+    } 
+} 
+echo '</table>' ; 
 
   ?>
 
